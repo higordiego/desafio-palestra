425387ee265e0ea2f45003fa7891f3cb233df032
Higor Diego <higordiegoti@gmail.com>
2016-10-07 19:43:10 -0300

    Corrigindo bug de espaco no html

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(minifyHTML({
 }));
 app.set('views', path.join(__dirname, 'app/views'));
 app.set('view engine', 'ejs');
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'rapadura.png')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 
